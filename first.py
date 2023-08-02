91bca99ccfeb1b73971fee3ec28520fefd6c3833
Naresh Khatri <111528185+nareshtestgithub@users.noreply.github.com>
2023-08-02 21:09:32 +0545

    remove the all multi-level comment

--- first.py
+++ first.py
@@ -6,32 +6,31 @@ c = a + b
 print("the sum of two numbers is:",c)
 
 # Taking input from the user  (We use the input method)..
-'''
+
 num1 =int(input("enter the number:"));
 num2 = int(input("Enter the second number:"))
 sum = num1 + num2;
 print("the sum of two number is :",sum)
-'''
+
 
 #Data type in python...
 # integer in python...
-"""""
+
 num1 = int(input("Enter the first number "))
 num2  = int(input("Enter the second number:"))
 sub = num1 - num2
 print("this difference is:",sub)
-"""""
+
 # float in python..
-""""
+
 num3 = float(input("Enter the third number: "))
 num4 = float(input("enter the fourth number:"))
 product = num3 * num4
 print("this multiplication is :" ,product)
-"""""
-# String in python...
 
-""""
-print("Your name is "+name)
+# String in python...
+name1 ="xyz"
+print("Your name is "+name1)
 
 fullname = input("What is your fullname?")
 print("hello " +fullname)
@@ -41,7 +40,7 @@ print("you are " +str(age)+ " years old")
 
 height = float(input("What's your height?:"))
 print("your heught "+str(height)+ " fit" )
-"""
+
 name = "naresh "
 lname =str.upper(name)
 print(lname)
@@ -63,6 +62,10 @@ print(len(b))
 print(len(b.center(80)))
 print(b.capitalize())
 
+if "mango" in b:
+    print("Yes")
+else:
+    print("No")
 # Boolean in python...
 
 
