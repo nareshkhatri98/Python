c5f17e6526feb6993153e2aebe71a0ad87d52d0f
Naresh Khatri <111528185+nareshtestgithub@users.noreply.github.com>
2023-09-26 14:45:05 +0545

    function in python

---
+++ function/firstfunction.py
@@ -0,0 +1,13 @@
+def Demo():
+       print("hello......")
+       print("This is a example of the function")
+
+Demo()
+
+def Addition () :
+      num1 = int(input("Enter the first number ::"))
+      num2 = int(input("Enter the second number::"))
+      return num1 +num2
+
+Result = Addition()
+print("Addition is", Result)
