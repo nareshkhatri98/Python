c7718805e69915a0034f8841bd07132e73eba700
Naresh Khatri <nareshkhatri9810@gmail.com>
2024-01-16 13:18:34 +0545

    Learn basic string function

--- string Formating/string.py
+++ string Formating/string.py
@@ -1,6 +1,31 @@
-name = "Naresh"
-paragraph = '''This is the multipline string 
-paragraph testing '''
+# name = "Naresh"
+# paragraph = '''This is the multipline string 
+# paragraph testing '''
 
-print(name)
-print(paragraph)
+# print(name)
+# print(paragraph)
+
+# python string indexing..
+
+fruit = "Apple"
+print(fruit[-1]) # python support the negative indexing
+print(fruit[4])
+my_char = fruit[2]
+print(my_char)
+
+#slicing  in string
+print(len(fruit))
+
+
+def my_function( x):
+    return x[::-1]
+ 
+reuslt =my_function("Hello everyone! this is Naresh Khatri")
+print(reuslt)
+
+my_strring = """ Hello guys  my name is Naresh Khatri"""
+
+#String reverse
+my_name  = "naresh"
+print(my_name[::-1])
+print(my_name.upper())
