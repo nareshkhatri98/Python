58d8153bc4317615693c3cdf030a08212f9a5527
Naresh Khatri <nareshkhatri9810@gmail.com>
2024-01-16 12:32:53 +0545

    Add while loop

--- loops/range.py
+++ loops/range.py
@@ -1,2 +1,10 @@
 a = list(range(5, 12, 2))
 print(a)
+
+# for x in range(10) :
+#     print(2*x, end= " ")
+
+names = ['Naresh', 'hari', 'gita', 'sita']
+
+for name in names:
+    print(name)
---
+++ loops/whileloop.py
@@ -0,0 +1,11 @@
+# the while loop in python is used to iterate over a block of code as long as the test
+# expression(condition) is true.
+# we generally use this loop  when we don't know befoehand, the number of times to iterate.
+# while test_expression(condition):
+     # body of while
+
+
+n = 5
+while n>=0:
+    print(n)
+    n-=1
