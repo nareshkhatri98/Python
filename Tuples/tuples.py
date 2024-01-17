a67d34745355ad4419c6487f9eade6c101ed28de
Naresh Khatri <nareshkhatri9810@gmail.com>
2024-01-17 14:53:10 +0545

    Learn the tuples

---
+++ Tuples/tuples.py
@@ -0,0 +1,7 @@
+# a tuples is created by placing all the elements inside parenthess().
+# Tuples are immutable. that means the elemets of a tuple cannot be changed once it has been assigned.
+
+
+my_tuple= (12, 14 , 25)
+print(my_tuple)
+print(sum(my_tuple))
