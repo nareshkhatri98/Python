689a7ebfb9f89d6b205a4b85a85fff299be1fed1
Naresh Khatri <nareshkhatri9810@gmail.com>
2024-01-17 15:35:00 +0545

    Learn the module in the python

---
+++ function/Module/Math.py
@@ -0,0 +1,11 @@
+import math as m
+
+print(m.pi)
+print(m.sin(45))
+
+#  to mport the  particular math function.
+print("To import the specific math functions")
+from math import sin, cos, log2
+print(sin(3.14))
+print(cos(3.14))
+print(log2(3.14))
