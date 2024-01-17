539075acfa5a908d7460271e2a21246192b7294b
Naresh Khatri <nareshkhatri9810@gmail.com>
2024-01-17 14:54:39 +0545

    for loop in list

--- lists/listfunction.py
+++ lists/listfunction.py
@@ -18,3 +18,13 @@ s = range( 1, 17)
 print((list(s)))
 
 print(sum(my_list))
+
+# for loop in list
+
+Numbers = [0,1,2,3,4,5,6,7,8,9]
+for element in Numbers:
+    if(element% 2 ==1):
+        print(element)
+
+for element in Numbers:
+    print(element **2, end =" ")
