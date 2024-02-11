7a0522c23aaee5eb60d79c0d32233b20016ad322
Naresh Khatri <nareshkhatri9810@gmail.com>
2024-02-11 18:39:06 +0545

    learn the finally keyword

--- oop/exception.py
+++ oop/exception.py
@@ -3,7 +3,7 @@
 # example of the exception handiling....
 def Multiplication():
     try:
-     number = input("Enter the number")
+     number = input("Enter the number: ")
      print(f"Multiplication table of {number}: ")
 
      for i in range(1,11):
@@ -16,10 +16,24 @@ Multiplication()
 
 def DividedByZero():
   try:
-    number = int(input("Enter the number do you want"))
+    number = int(input("Enter the number do you want: "))
     result = number/0
     print("The result is: ",result)
   except Exception as e:
     print("Invalid opertion ",e)
 
 DividedByZero()
+
+def DemoFunction():
+  try:
+    list = [1,2,3,5,6,7,8]
+    index = int(input("Enter the index: "))
+
+    print(list[index])
+  except Exception as e:
+    print(e)
+    # the code inside the finally block it is always executed...
+  finally:
+    print("I am always executed....")
+
+DemoFunction()
