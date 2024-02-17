96c72728043a09c81e62b01ff64375d7415ebdec
Naresh Khatri <nareshkhatri9810@gmail.com>
2024-02-17 22:23:18 +0545

    creat the miniproject

---
+++ project/Guessthnumber.py
@@ -0,0 +1,22 @@
+import random
+
+Target = random.randint(1,10)
+
+
+while True:
+   userChoice = int(input("Guess the Number or Quit(Q): "))
+   if(userChoice =='Quite'):
+      break
+   if(userChoice == Target):
+      print("Success : Correct Guess!!!")
+      break
+
+   if(userChoice<=Target):
+      print("Guess number is smaller than the target")
+
+   if(userChoice>=Target):
+      print("Guess numbe is grather than target")
+         
+
+
+print("GAME OVER -----------")
