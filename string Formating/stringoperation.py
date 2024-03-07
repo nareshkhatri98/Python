f72d400e2a8f835eb16a0056eff69a493a6762f4
Naresh Khatri <nareshkhatri9810@gmail.com>
2024-03-07 16:21:27 +0545

    learn the basic string operation

---
+++ string Formating/stringoperation.py
@@ -0,0 +1,29 @@
+Name = "Naresh"
+# find out the length of the string..
+print(len(Name))
+
+# To concatenation of the two string..
+# usse use '+' sign..
+
+fname = "Naresh"
+lname = "Khatri"
+fullname = fname + ' ' +lname
+print(fullname)
+print(fname + ' ' +lname)
+
+# To reverse the string.
+str1 = "hello world this is naresh Khatri"
+print(str1[::-1])
+
+# This is used for the split the string the different part.
+print(str1[3:10])
+
+str2 = "NARESH"
+str3 = 'khatri'
+
+print(str2.lower())
+print(str3.upper())
+print(str1.count("a"))
+print(str1.endswith("sh"))
+print(str1.capitalize())
+print(str2.split( " "))
