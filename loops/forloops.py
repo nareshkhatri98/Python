cb463c71d22a79b1affc198d94b1428f2c0e3425
Naresh Khatri <nareshkhatri9810@gmail.com>
2024-03-21 13:56:07 +0545

    solve the pattern question

--- loops/forloops.py
+++ loops/forloops.py
@@ -15,3 +15,20 @@ for i in range(1,11):
     multi = number * i
 
     print(number, "*", i, "= ",multi )
+
+
+
+number = int(input("Enter the row do you want"))
+for i in range(1, number+1):
+    for j in range(1, i+1):
+        print("*", end="")
+    print()
+
+
+number1 = int(input("Enter the row do you want"))
+
+for i in range(number, 0, -1):
+    for j in range(0,i):
+        print("*", end="")
+    print()
+
