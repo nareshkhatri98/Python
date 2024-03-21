898c22603918ecb5dd26569799107396325b399e
Naresh Khatri <nareshkhatri9810@gmail.com>
2024-03-21 13:42:03 +0545

    Learn the loops

---
+++ loops/forloops.py
@@ -0,0 +1,17 @@
+name = "Naresh"
+for i in name:
+    print(i)
+
+
+
+fullName = "Jhon Jhon"
+for j in range(15):
+    print(fullName)
+
+
+#  create a multiply table in python
+number = int(input("Enter the number do you want to calculate the multiplicatiion table: "))
+for i in range(1,11):
+    multi = number * i
+
+    print(number, "*", i, "= ",multi )
